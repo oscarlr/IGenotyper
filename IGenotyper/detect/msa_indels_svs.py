97cbbe2c6836fcb6fe2ec107a854c0d5f7612bf2
oscarlr <olrodriguez01@gmail.com>
2021-02-11 14:53:16 -0500

    add

--- IGenotyper/detect/msa_indels_svs.py
+++ IGenotyper/detect/msa_indels_svs.py
@@ -3,7 +3,7 @@ import json
 import pysam
 from pybedtools import BedTool
 
-from IGenotyper.common.helper import phased_blocks_merged_seq,assembly_coords,get_ref_seq,skip_read
+from IGenotyper.common.helper import assembly_coords,get_ref_seq,skip_read,get_phased_blocks
 
 from IGenotyper.detect.msa_to_variants_without_hmm import path_to_variants
 
@@ -30,9 +30,10 @@ def filter_coords(files,contig_regions,phased_regions):
     return phased_contig_cov_filtered        
     
 def msa_coords(files):
-    phased_regions = phased_blocks_merged_seq(files)
+    #phased_regions = phased_blocks_merged_seq(files)
+    phased_regions = get_phased_blocks(files,files.phased_blocks)
     phased_regions = get_haps_coords(phased_regions)
-    contig_coords = assembly_coords(files.merged_assembly_to_ref_phased)
+    contig_coords = assembly_coords(files.assembly_to_ref_phased)
     contig_coords = get_haps_coords(contig_coords)
     filtered_coords = {}
     for hap in ["0","1","2"]:
@@ -45,7 +46,7 @@ def msa_coords(files):
 
 def extract_assembly_sequence(files,chrom,start,end,hap):
     hap_sequence = None
-    samfile = pysam.AlignmentFile(files.merged_assembly_to_ref_phased,'rb')
+    samfile = pysam.AlignmentFile(files.assembly_to_ref_phased,'rb')
     for contig in samfile.fetch(chrom,start,end):
         if skip_read(contig):
             continue
@@ -74,17 +75,20 @@ def extract_assembly_sequence(files,chrom,start,end,hap):
     return hap_sequence
 
 def extract_sequence(files,chrom,start,end,haps):
+    seq_blank = False
     outfasta = "%s/%s_%s_%s.fasta" % (files.msa_fasta,chrom,start,end)
     with open(outfasta,'w') as outfasta_fh:
         outfasta_fh.write(">ref\n%s\n" % get_ref_seq(files,chrom,start,end))
         for h in haps:
             seq = extract_assembly_sequence(files,chrom,start,end,h)
+            if len(seq) == 0:
+                seq_blank = True
             if h != "0":
                 outfasta_fh.write(">hap%s\n%s\n" % (h,seq))
             else:
                 outfasta_fh.write(">hap1\n%s\n" % seq)
                 outfasta_fh.write(">hap2\n%s\n" % seq)
-    return outfasta
+    return (outfasta,seq_blank)
 
 def call_variants(files,fastafile,chrom,start,end,command_line_tools,variants):
     msa_fn = "%s/%s_%s_%s.clu" % (files.msa_msa,chrom,start,end)
@@ -108,7 +112,9 @@ def detect_msa_variants(files,sample,command_line_tools):
             chrom = str(coord[0])
             start = int(coord[1])
             end = int(coord[2])            
-            fastafile = extract_sequence(files,chrom,start,end,haps)
+            fastafile,seq_blank = extract_sequence(files,chrom,start,end,haps)
+            if seq_blank:
+                continue
             call_variants(files,fastafile,chrom,start,end,command_line_tools,variants)
     variants.sort(key=lambda x: int(x[2]))
     header = ["chrom","start","end","event","genotype","size","ref","hap1","hap2","seq_start","seq_end","msa"]

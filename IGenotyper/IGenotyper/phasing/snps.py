0f89418c40e2a82c1757bd74022cb523a5c3b99f
Uddalok Jana <uddalok06@gmail.com>
2023-12-21 12:27:59 -0500

    removing check functions for pacbio machine type and proceeding as SEQUELII for REVIO as well, this will not work with pacbio machine before SEQUELII

--- IGenotyper/IGenotyper/phasing/snps.py
+++
@@ -1,9 +0,0 @@
-#!/bin/env python
-
-from IGenotyper.command_lines.snps import Snps
-
-def generate_phased_snps(files,cpu,sample):
-    snps_command_line = Snps(files,cpu,sample)
-    snps_command_line.snp_candidates_from_ccs()
-    snps_command_line.snp_genotypes_from_ccs()
-    snps_command_line.phase_ccs_snvs()
